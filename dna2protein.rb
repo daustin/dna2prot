1f8b963924ee1f0c51b0e051ea8c17408015de3d
David Austin <daustin@MEATH.GCRC.UPENN.EDU>
2009-01-30 16:49:19 -0500

    Beta version. does a full 6 frame translation from NA to AA.  tailored for chromosome inputs.

--- dna2protein.rb
+++ dna2protein.rb
@@ -1,25 +1,38 @@
 require 'rubygems'
 require 'bio'
- 
-outfile = File.open("test.fasta", "w+")
+require 'strscan'
 
+MIN_LENGTH = 5
 
 file = Bio::FastaFormat.open(ARGV.shift)
 file.each do |entry|
   # do something on each fasta sequence entry
-  seq = entry.naseq
-  # seq = Bio::Sequence::NA.new("atcggtcggctta" * 10)
+  chrom = entry.naseq
+  # puts chrom.seq #debug
+  ubound = chrom.seq.length - 1 # upper bound to reverse position for frames 4 5 6
+  
+  1.upto(6) do |frame|
+    
+    chrom_trans = StringScanner.new(chrom.translate(frame,1,'N')) # our string scanner
+    
+    last_pos = 0
+    while ! (aa_seq = chrom_trans.scan_until(/\*/)).nil?
+      aa_seq.chomp!('*')
+      start_pos = (last_pos*3)+frame-1
+      stop_pos = start_pos+(aa_seq.length*3)+2 # add 2 so it includes the last two NA spots
+      if frame > 3
+        #switch positions if on a reversed frame
+        start_pos = ubound - start_pos
+        stop_pos = ubound - stop_pos
+      end
+      temp_seq = Bio::Sequence::AA.new(aa_seq)
+      puts temp_seq.to_fasta("#{entry.entry_id}#{entry.comment}_#{frame}_#{start_pos}_#{stop_pos} Chromosome #{entry.entry_id.match(/\d+/)} Frame #{frame} start=#{start_pos} end=#{stop_pos}",60) unless aa_seq.length < MIN_LENGTH #unless a small AA
+      last_pos = chrom_trans.pos
 
-  seq.reverse_complement!
-
-  1.upto(3) do |i|
-    puts i
-    # outfile.write(seq.translate(i,1,'N').to_fasta("test",60))
-    puts seq.translate(i,1,'N').to_fasta("#{entry.entry_id} #{entry.comment} TRANS #{i}",60)
+    end
+    
   end
-
-
+  
 end
 
 
-outfile.close
